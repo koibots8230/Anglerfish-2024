1e21a7b5bcb5f5b2ee1bf33a8ce9bf10210c8117
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2024-01-27 13:00:01 -0500

    Shooter Pivot IO code builds

--- src/main/java/com/koibots/robot/subsystems/ShooterPivotIOSim.java
+++ src/main/java/com/koibots/robot/subsystems/ShooterPivotIOSim.java
@@ -1,4 +1,46 @@
 package com.koibots.robot.subsystems;
 
 public class ShooterPivotIOSim implements ShooterPivotIO{
+
+    @Override
+    public void updateInputs(ShooterPivotIOInputs inputs) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public double getShooterPosition() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void zeroShooterPositionOffset() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public double getShooterPivotOutputCurrent() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setShooterPivotMotorSpeed(double desiredPosition) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setShooterPivotBrakeMode() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setShooterPivotCoastMode() {
+        // TODO Auto-generated method stub
+        
+    }
 }
