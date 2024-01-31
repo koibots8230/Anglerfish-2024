8c7990235b2ca8275939041cbc71b108488c29b7
Caleb O'Neal <caleboneal07@gmail.com>
2024-01-31 16:19:53 -0500

    Switched code to use rio wiring

--- src/main/java/com/koibots/robot/subsystems/intake/IntakePivot/IntakePivotIOSparkMax.java
+++ src/main/java/com/koibots/robot/subsystems/intake/IntakePivot/IntakePivotIOSparkMax.java
@@ -5,15 +5,17 @@ import com.revrobotics.SparkAbsoluteEncoder.Type;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 
-import com.koibots.robot.Constants.IntakeConstants;
-import com.revrobotics.AbsoluteEncoder;
+import com.koibots.robot.Constants.IntakeConstants;;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.CounterBase;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Encoder;
 
 public class IntakePivotIOSparkMax implements IntakePivotIO {
     private final CANSparkMax intakePivotSparkMax;
-    private final AbsoluteEncoder intakePivotEncoder;
+    private final DutyCycleEncoder intakePivotEncoder;
 
     private final boolean isIntakePivotMotorInverted = true;
 
@@ -21,13 +23,15 @@ public class IntakePivotIOSparkMax implements IntakePivotIO {
         intakePivotSparkMax = new CANSparkMax(intakePivotId, MotorType.kBrushless);
         intakePivotSparkMax.restoreFactoryDefaults();
         intakePivotSparkMax.setCANTimeout(250);
-        intakePivotEncoder = intakePivotSparkMax.getAbsoluteEncoder(Type.kDutyCycle);
+        intakePivotEncoder = new DutyCycleEncoder(0);
 
         /* Apply position and velocity conversion factors for the pivoting encoder.
          * This will be in radians and radians per second.
         */
-        intakePivotEncoder.setPositionConversionFactor(IntakeConstants.INTAKE_PIVOT_ENCODER_POSITION_FACTOR);
-        intakePivotEncoder.setVelocityConversionFactor(IntakeConstants.INTAKE_PIVOT_ENCODER_VELOCITY_FACTOR);
+        //intakePivotEncoder.setPositionConversionFactor(IntakeConstants.INTAKE_PIVOT_ENCODER_POSITION_FACTOR);
+        //intakePivotEncoder.setVelocityConversionFactor(IntakeConstants.INTAKE_PIVOT_ENCODER_VELOCITY_FACTOR);
+
+        intakePivotEncoder.setPositionOffset(0);
 
         intakePivotSparkMax.setInverted(isIntakePivotMotorInverted);
         intakePivotSparkMax.setSmartCurrentLimit(20);
@@ -40,8 +44,8 @@ public class IntakePivotIOSparkMax implements IntakePivotIO {
 
     @Override
     public void updateInputs(IntakePivotInputs inputs) {
-        inputs.intakePivotPosition = Rotation2d.fromRotations(intakePivotEncoder.getPosition());
-        inputs.intakePivotVelocityRadPerSec = Units.rotationsPerMinuteToRadiansPerSecond(intakePivotEncoder.getVelocity());
+        inputs.intakePivotPosition = Rotation2d.fromRotations(intakePivotEncoder.get() * 2 * Math.PI);
+        inputs.intakePivotVelocityRadPerSec = Units.rotationsPerMinuteToRadiansPerSecond(0);
         inputs.intakePivotAppliedVolts = intakePivotSparkMax.getAppliedOutput() * intakePivotSparkMax.getBusVoltage();
         inputs.intakePivotCurrentAmps = new double[] { intakePivotSparkMax.getOutputCurrent() };
     }
