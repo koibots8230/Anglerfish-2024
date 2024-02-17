325a2626c58d2f2b04d91b2412a230aa52d0abe1
JakeStew81 <jakestewart711@icloud.com>
2024-02-17 11:39:10 -0800

    Actually did prev. commit

---
+++ src/main/java/com/koibots/robot/autos/SysID.java
@@ -0,0 +1,50 @@
+// Copyright (c) 2024 FRC 8230 - The KoiBots
+// https://github.com/koibots8230
+
+package com.koibots.robot.autos;
+
+import static com.koibots.robot.subsystems.Subsystems.*;
+
+import com.koibots.lib.sysid.SysIDMechanism;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import java.util.function.BooleanSupplier;
+import org.littletonrobotics.junction.Logger;
+
+public class SysID extends SequentialCommandGroup {
+
+    public SysID(SysIDMechanism mechanism, BooleanSupplier nextButton) {
+        SysIdRoutine sysid =
+                new SysIdRoutine(
+                        new SysIdRoutine.Config(
+                                null,
+                                null,
+                                null,
+                                (state) -> Logger.recordOutput("SysIdTestState", state.toString())),
+                        switch (mechanism) {
+                            case Elevator -> new SysIdRoutine.Mechanism(
+                                    (voltage) -> Elevator.get().setVoltage(voltage),
+                                    null,
+                                    Elevator.get());
+                            case Swerve -> new SysIdRoutine.Mechanism(
+                                    (voltage) -> Swerve.get().setDriveVoltages(voltage),
+                                    null,
+                                    Swerve.get());
+                        });
+        addCommands(
+                sysid.quasistatic(SysIdRoutine.Direction.kForward),
+                new WaitUntilCommand(nextButton),
+                sysid.quasistatic(SysIdRoutine.Direction.kReverse),
+                new WaitUntilCommand(nextButton),
+                sysid.dynamic(SysIdRoutine.Direction.kForward),
+                new WaitUntilCommand(nextButton),
+                sysid.dynamic(SysIdRoutine.Direction.kForward));
+
+        addRequirements(
+                switch (mechanism) {
+                    case Elevator -> Elevator.get();
+                    case Swerve -> Swerve.get();
+                });
+    }
+}
---
+++ src/main/java/com/koibots/robot/commands/Shooter/SpinUpShooter.java
@@ -0,0 +1,28 @@
+// Copyright (c) 2024 FRC 8230 - The KoiBots
+// https://github.com/koibots8230
+
+package com.koibots.robot.commands.Shooter;
+
+import static com.koibots.robot.subsystems.Subsystems.Shooter;
+
+import edu.wpi.first.units.*;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class SpinUpShooter extends Command {
+
+    Measure<Velocity<Angle>> speed;
+
+    public SpinUpShooter(Measure<Velocity<Angle>> speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public void initialize() {
+        Shooter.get().setVelocity(speed);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Shooter.get().atSetpoint();
+    }
+}
