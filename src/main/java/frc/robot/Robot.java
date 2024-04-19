2f410dbbff5bd2bf34e49d24992cd9f77a16579a
Toby Mickelson <tmickelson@tinyfern.com>
2024-04-19 14:03:09 -0400

    initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,88 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot
+{
+    private Command autonomousCommand;
+    
+    private RobotContainer robotContainer;
+    
+    
+    @Override
+    public void robotInit() {}
+    
+    
+    @Override
+    public void robotPeriodic()
+    {
+        CommandScheduler.getInstance().run();
+    }
+    
+    
+    @Override
+    public void disabledInit() {}
+    
+    
+    @Override
+    public void disabledPeriodic() {}
+    
+    
+    @Override
+    public void disabledExit() {}
+    
+    
+    @Override
+    public void autonomousInit()
+    {
+        autonomousCommand = robotContainer.getAutonomousCommand();
+        
+        if (autonomousCommand != null)
+        {
+            autonomousCommand.schedule();
+        }
+    }
+    
+    
+    @Override
+    public void autonomousPeriodic() {}
+    
+    
+    @Override
+    public void autonomousExit() {}
+    
+    
+    @Override
+    public void teleopInit()
+    {
+        if (autonomousCommand != null)
+        {
+            autonomousCommand.cancel();
+        }
+    }
+    
+    
+    @Override
+    public void teleopPeriodic() {}
+    
+    
+    @Override
+    public void teleopExit() {}
+    
+    
+    @Override
+    public void testInit()
+    {
+        CommandScheduler.getInstance().cancelAll();
+    }
+    
+    
+    @Override
+    public void testPeriodic() {}
+    
+    
+    @Override
+    public void testExit() {}
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+
+
+public class RobotContainer
+{
+    public RobotContainer()
+    {
+        configureBindings();
+    }
+    
+    
+    private void configureBindings() {}
+    
+    
+    public Command getAutonomousCommand()
+    {
+        return Commands.print("No autonomous command configured");
+    }
+}
